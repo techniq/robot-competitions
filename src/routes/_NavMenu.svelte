1c6def4bba1bcf5297242e8011dc1264eaffabfb
Sean Lynch <techniq35@gmail.com>
2022-04-25 07:37:25 -0400

    Add menu and rename teams route

---
+++ src/routes/_NavMenu.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { mdiAccountGroup, mdiCodeBraces, mdiHome } from '@mdi/js';
+	import { NavItem } from 'svelte-ux';
+</script>
+
+<NavItem text="Home" icon={mdiHome} path="/" class="pl-6 py-2 mt-2" />
+
+<div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">General</div>
+<NavItem text="Teams" icon={mdiAccountGroup} path="/teams" class="pl-6 py-2" />
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { AppLayout } from 'svelte-ux';
+	import NavMenu from './_NavMenu.svelte';
 
 	import '../app.css';
 </script>
 
 <AppLayout>
+	<nav slot="nav" class="nav h-full">
+		<NavMenu />
+	</nav>
+
 	<slot />
 </AppLayout>
