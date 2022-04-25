adbbfba1d2400e9bf3b9c3078ec4bbfbedd006f9
Sean Lynch <techniq35@gmail.com>
2022-04-25 07:45:44 -0400

    Move data to lib

--- src/routes/teams.js
+++ src/routes/teams.js
@@ -4,7 +4,7 @@ import { csvParse, autoType } from 'd3-dsv';
 
 /** @type {import('../../.svelte-kit/types/src/routes/index.json').RequestHandler} */
 export async function get(event) {
-	const csvPath = fileURLToPath(new URL('divisions.csv', import.meta.url));
+	const csvPath = fileURLToPath(new URL('../lib/data/divisions/divisions.csv', import.meta.url));
 	const csvFile = await readFile(csvPath);
 	const teamsWithDivisions = csvParse(csvFile.toString(), autoType);
 
